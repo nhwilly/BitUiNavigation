23464df87554026765353f109fa5b9c74cb4635f
nhwilly <billnoel@outlook.com>
2025-08-06 18:33:03 -0400

    save

--- BitUiNavigation/BitUiNavigation.Client/Pages/Modals/IModalSectionGuard.cs
+++ BitUiNavigation/BitUiNavigation.Client/Pages/Modals/IModalSectionGuard.cs
@@ -3,4 +3,9 @@ public interface IModalSectionGuard
 {
     Task<bool> CanNavigateAwayAsync();
 }
+public interface ISupportsSaveOnNavigate
+{
+    Task SaveOnNavigateAsync();
+
+}
 public delegate void ModalGuardRegistration(IModalSectionGuard component);
